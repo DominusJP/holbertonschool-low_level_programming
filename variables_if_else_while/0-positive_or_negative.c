6949c806b7b8e8acd0417f1623355fb8fca49838
DominusJP <6775@holbertonstudents.com>
2023-06-15 09:37:36 -0700

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0) {printf("%d is positive\n", n)}:
-	else if (n = 0) {ptintf("%d is zeroe\n", n)}:
-	else {printf("%d is negative\n", n)}:
+	if (n > 0) {printf("%d is positive\n", n):}
+	else if (n = 0) {ptintf("%d is zeroe\n", n):}
+	else {printf("%d is negative\n", n):}	   
 
 	return (0);
 }
