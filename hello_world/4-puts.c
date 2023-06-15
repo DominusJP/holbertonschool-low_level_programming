648eb1afe79f6c14a6550b1143ab4b641a7c2d0a
DominusJP <6775@holbertonstudents.com>
2023-06-15 06:34:46 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,16 +1,4 @@
-/**
- *  * File: 4-puts.c
- *   * Auth: DominusJP <https://github.com/DominusJP>
- *    */
-
 #include <stdio.h>
-
-/**
- *  * main - Prints "Programming is like building a multilingual
- *   *                puzzle, followed by a new line.
- *    *
- *     * Return: Always 0.
- *      */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
