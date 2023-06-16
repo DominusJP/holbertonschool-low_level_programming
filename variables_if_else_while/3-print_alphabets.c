cd62a22a6b5da2fce870afac9321222ae6920789
DominusJP <6775@holbertonstudents.com>
2023-06-16 05:34:52 -0700

    3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	har alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 	for (i = 0; i < 52; i++)
 	{
