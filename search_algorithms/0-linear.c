c3bdeba8780c07d4bb6823032fe0674dd4796f84
DominusJP <6775@holbertonstudents.com>
2023-10-02 15:55:22 -0700

    0.1

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,19 +11,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
-
     if (array == NULL)
-        return (-1);
+        return (-1); // Return -1 if the array is NULL
 
-    for (i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
     {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
         if (array[i] == value)
-            return (i);
+            return (i); // Return the index if the value is found
     }
 
-    return (-1); // Value not found
+    return (-1); // Return -1 if the value is not found
 }
 
